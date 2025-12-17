c1c56c3c51c8431fae186a8ca410a38e63243c30
Ubuntu <ubuntu@ip-172-31-29-10.ec2.internal>
2025-12-17 08:54:22 +0000

    script 4

---
+++ adduser_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ set -e
+
+  createUser(){
+    
+	    read -p "Enter name: " name
+	    read -s -p "Enter pasword: " pass
+	    echo "creating user $name ..."
+	   sudo useradd $name
+	    echo "$name:$pass" | sudo chpasswd
+	   echo "user $user  create succesfully"
+   
+   }
+   createUser
